04f0311e73714ee1977f9769d56d7a3659ff4aea
Antoine Colombier <antoine38100@gmail.com>
2019-03-24 00:09:29 +0100

    Adding `Gravity` field on the Bird to control difficulty

--- Assets/Script/Bird.cs
+++ Assets/Script/Bird.cs
@@ -8,39 +8,39 @@ public class Bird : MonoBehaviour
     public float Speed = 50f;
     public float JumpBoost = 250f;
     public float CoolDownTime = 2;
+    public float Gravity = 1f;
     
     private Rigidbody rigitBody;
-    private float knockedOut = 0f;
+    private float knockedOutAt;
 
     // Start is called before the first frame update
     void Awake()
     {
+        knockedOutAt = 0;
         rigitBody = GetComponent<Rigidbody>();
-
     }
 
     void Start()
     {
         rigitBody.velocity = new Vector3(Speed, 0, 0);
+        Physics.gravity = new Vector3(0, -Gravity, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (knockedOut == 0)
+        if (knockedOutAt == 0)
         {
             if (rigitBody.velocity.x != Speed || transform.position.y < -6.5f)
             {
-                knockedOut = Time.fixedTime;
-
-                Debug.Log("KO");
+                knockedOutAt = Time.fixedTime;
             }
             else if (Input.GetButtonDown("Jump"))
             {
                 rigitBody.velocity += new Vector3(0, JumpBoost, 0);
             }
         }
-        else if (knockedOut != 0 && knockedOut + CoolDownTime < Time.fixedTime)
+        else if (knockedOutAt != 0 && knockedOutAt + CoolDownTime < Time.fixedTime)
         {
             SceneManager.LoadScene(0);
         }
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -13,10 +13,17 @@ public class GameManager : MonoBehaviour
 
     private ArrayList Gates = new ArrayList();
     private int gatePassed = 0;
+    private UnityEngine.UI.Text gatesLabel;
+
+    private void Awake()
+    {
+        gatesLabel = GateCounter.GetComponent<UnityEngine.UI.Text>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        Gates.Clear();
         SpawnGate();
     }
 
@@ -28,12 +35,10 @@ public class GameManager : MonoBehaviour
 
         Gates.Insert(0, gate);
 
-        for (int g = Gates.Count - 1; g > 0; --g)
+        if (Gates.Count > 3)
         {
-            gate = (Gate)Gates[g];
-            if (!gate.readyToDelete)
-                break;
-            Destroy(gate.gameObject);
+            Destroy(((Gate)Gates[3]).gameObject);
+            Gates.RemoveAt(3);
         }        
     }
 
@@ -45,7 +50,7 @@ public class GameManager : MonoBehaviour
             gatePassed++;
             SpawnGate();
         }
-        GateCounter.GetComponent<UnityEngine.UI.Text>().text = gatePassed.ToString();
+        gatesLabel.text = gatePassed.ToString();
 
     }
 }
